075266391238ae25e858a3c91524c6c65f8e36dc
kayleelo <127044819+kayleelo@users.noreply.github.com>
2023-03-05 17:08:23 -0800

    feed changes

--- hackApp/src/Pages/feed.jsx
+++ hackApp/src/Pages/feed.jsx
@@ -1,16 +1,32 @@
+import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
-import * as React from 'react';
-import {firebase} from '../Firebase/firebase';
+import { Image, View, TouchableOpacity } from 'react-native'
+import { Text, Avatar, withStyles, List } from 'react-native-ui-kitten'
 
 function FeedPage() {
     return (
-      console.log(firebase);
-      
       <View style={styles.feedStyle}>
         <Text>FEED</Text>
       </View>
     )
 }
+
+const Posts = [
+  {
+    id: 1,
+    imageURL:
+      'https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg',
+    nameHolder:
+      'testing'
+  },
+  {
+    id: 2,
+    imageURL:
+      'https://media.glamour.com/photos/5ea89429e67f360008b064d8/master/w_1172,h_1412,c_limit/Pink%20Anthurium.png',
+    nameHolder:
+      'testing again'
+  }
+]
 
 const styles = StyleSheet.create({
     feedStyle: {
@@ -20,5 +36,14 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
+
+return (
+  <List
+    style={this.props.themedStyle.container}
+    data={Posts}
+    renderItem={renderItem}
+    keyExtractor={Posts.id}
+  />
+)
 
 export default FeedPage;
